7a4afa4453e98a1a29988fb3abdb3af820d4abaf
Jennifer Busca <jennifer.busca@gmail.com>
2019-03-27 19:29:53 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -15,6 +15,7 @@ def loop_iterator(number_of_times)
       counter += 1
     break if counter == number_of_times
   end
+end
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
